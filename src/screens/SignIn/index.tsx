54f417cbd751bc04cc4174aa273afe7aabb35573
Sander <sanderdsz@gmail.com>
2022-10-05 16:00:36 -0300

    sign-in validation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,9 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
+import * as Yup from "yup";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
@@ -16,6 +18,28 @@ export function SignIn() {
   const [password, setPassword] = useState("");
 
   const theme = useTheme();
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("A senha é obrigatória"),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert("Erro na autenticação", error.message);
+      } else {
+        return Alert.alert(
+          "Erro na autenticação",
+          "Ocorreu um erro ao realizar o login, tente novamente mais tarde"
+        );
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -56,8 +80,8 @@ export function SignIn() {
           <Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
               color={theme.colors.main}
             />
